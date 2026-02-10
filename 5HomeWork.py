f053ca208a4a4ac37b58e844d4fbfc91fe288b1f
Rassul100 <n.klyshbekova@hyundai.kz>
2026-02-10 23:09:55 +0500

    Update 5HomeWork.py

--- 5HomeWork.py
+++ 5HomeWork.py
@@ -1,7 +1,30 @@
-for i in range (1,4):
-    for j in range (1,11):
-        if i==j:
-            continue
-        print (i,'x',j,'=',i*j)
-  
+#Первое задание
+a = int(input("Введите число: "))
+
+d = 0
+
+for i in range(10, a + 1):
+    d += i
+
+print("Сумма чисел:", d)
+
+#Второе задание
+a = int(input("Введите число: "))
+
+for d in range(0, a+1):
+if d % 2 == 0:
+    print(d)
+    
+#Третье задание
+n = int(input("Введите число: "))
+
+summa = 0
+
+for i in range(0, n+1):
+    if i % 3 == 0:
+        summa += i
+
+
+print("Сумма чисел:", summa)
+
     
